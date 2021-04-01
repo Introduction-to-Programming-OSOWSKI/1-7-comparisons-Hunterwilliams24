599590834aa149abdeeea15aa5f1994ef06b4057
Williams Hunter <820211@stma.k12.mn.us>
2021-04-01 10:20:32 -0500

    comparisons

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-#WRITE YOUR CODE IN THIS FILE
+def greaterThan(6, 10):
+    return("False")
+
+def equalTo(23, 23):
+    return("True")
+
+def lessOrEqual(99, 100):
+    return("True")
+
+def lessOrEqual(100, 100):
+    return("True")
+
+def lessOrEqual(101, 100):
+    return("False")
